7c42c7ee094aea0f26e8f399d6c43aec449eb70c
SkwarekDev <skwarek.develop@gmail.com>
2025-11-30 15:06:42 +0100

    Game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,10 +1,9 @@
 package main;
 
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     // SCREEN SETTINGS
     private final int ORIGINAL_TILE_SIZE = 16; // 16 x 16 tile
@@ -18,9 +17,38 @@ public class GamePanel extends JPanel {
     private final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COLUMN; // 768 px
     private final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 576 px
 
+    private Thread gameThread;
+
     public GamePanel(){
+
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null){
+
+            // 1. UPDATE: update game logic such as character position
+            update();
+
+            // 2. DRAW: draw the screen with the updated game logic
+            repaint();
+        }
+    }
+
+    public void update(){}
+
+    public void paintComponent(Graphics graphics){
+
+        super.paintComponent(graphics);
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
     public static void main(String[] args){
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
@@ -17,5 +18,7 @@ public class Main {
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
